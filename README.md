ee761c4682420527b3bd6fb9991f3014245d43d1
Kyle Byungsu Min <kylemin@umich.edu>
2018-12-01 02:57:24 -0500

    Now it can iterate over multiple directories

--- README.md
+++ README.md
@@ -1,66 +1,32 @@
 GPU based optical flow extraction from videos
 =================================================
-Forked from https://github.com/feichtenhofer/gpu_flow by Antonino Furnari
-
-[![Build Status](https://travis-ci.org/dl-container-registry/furnari-flow.svg?branch=master)](https://travis-ci.org/dl-container-registry/furnari-flow)
-[![Docker Hub](https://img.shields.io/badge/hosted-dockerhub-22b8eb.svg)](https://hub.docker.com/r/willprice/furnari-flow/)
-[![Singularity Hub](https://www.singularity-hub.org/static/img/hosted-singularity--hub-%23e32929.svg)](https://singularity-hub.org/collections/575)
 
 ### Features
-The tool allows to extract optical flow from video. By default, the tool resizes
-the input video so that its height matches 256 pixels and aspect ratio remains
-the same. The tool creates a video where x and y optical flow images are stored
-side by side. Optical flow is obtained by clipping large displacement. Other
-options are available modifying the source code (see
-https://github.com/feichtenhofer/gpu_flow).
+This tool allows to extract optical flow from image sequences. The tool creates a video where x and y optical flow images are stored side by side. Optical flow is obtained by clipping large displacement. Other options are available, such as dilatation (how much to skip for calculating each optical flow).
 
 ### Dependencies:
  * [OpenCV 2.4](http://opencv.org/downloads.html)
  * [cmake](https://cmake.org/)
 
 ### Installation
-First, build opencv with gpu support. To do so, download opencv 2.4.x sources
-from https://opencv.org/releases.html. Unzip the downloaded archive, then enter
-the opencv folder and issue the following commands:
+First, build opencv 2.4.x with gpu support. To do so, download opencv 2.4.x sources from https://opencv.org/releases.html. Unzip the downloaded archive, then enter the opencv folder and enter the following commands:
 
  * `mkdir build`
  * `cd build`
  * `cmake -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF ..`
  * `make`
 
-Then clone the current repository and enter the `compute_flow_video` folder. Type:
+Then clone the current repository. Type:
 
- * `export OpenCV_DIR=path_to_opencv_build_directory`
+ * `export OpenCV_DIR=path_to_opencv_build_directory` (not required if you `make install` the opencv)
  * `mkdir build`
  * `cd build`
  * `cmake -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF ..`
  * `make`
 
 ### Usage
-The software assumes that all video frames have been extracted in a directory. Files should be named according to some pattern, e.g., `img_%07d.jpg`. The software will put flow files in the same directory using a provided filename pattern, e.g., `flow_%s_%07d.jpg`, where the %s will be subsituted with "x" for the x flows and "y" for the y flows. For example, if DIR is a directory containing 4 images:
-
-DIR:
-
- * `img_0000001.jpg`
- * `img_0000002.jpg`
- * `img_0000003.jpg`
- * `img_0000004.jpg`
-
-the command `compute_flow DIR img_%07d.jpg flow_%s_%07d.jpg` will read the images in order and compute optical flows. The content of DIR will be as follows after the execution of the command:
-
-DIR:
-
- * `img_0000001.jpg`
- * `img_0000002.jpg`
- * `img_0000003.jpg`
- * `img_0000004.jpg`
- * `flow_x_0000001.jpg`
- * `flow_x_0000002.jpg`
- * `flow_x_0000003.jpg`
- * `flow_x_0000004.jpg`
- * `flow_y_0000001.jpg`
- * `flow_y_0000002.jpg`
- * `flow_y_0000003.jpg`
- * `flow_y_0000004.jpg`
-
-where `flow_x_{n}.jpg` is the x flow computed between `img_{n}.jpg` and `img_{n+1}.jpg` (if no dilation is used - see help).
+The software assumes that all video frames have been extracted in a directory. Files should be named according to some pattern, e.g., `img_%07d.jpg`.
+```
+./compute_flow /z/dat list_dir_1.txt 0
+```
+The above command will read /z/dat/list_dir_1.txt file line by line. Then, it will read an image sequence of /z/dat/Kinetics-400-frames/line1 and save optical flow /z/dat/Kinetics-400-flow/line1/u and /z/dat/Kinetics-400-flow/line1/v.
--- src/compute_flow.cpp
+++ src/compute_flow.cpp
@@ -1,13 +1,12 @@
 //************************************************************************
 // compute_flow.cpp
-// Computes OpenCV GPU Brox et al. [1] and Zach et al. [2] TVL1 Optical Flow
-// Dependencies: OpenCV and Qt5 for iterating (sub)directories
-// Author: Christoph Feichtenhofer -> Forked by Antonino Furnari
+// Computes OpenCV GPU Zach et al. [1] TVL1 Optical Flow
+// Dependencies: OpenCV (compiled for GPU)
+// Author: Christoph Feichtenhofer -> Forked by Antonino Furnari -> again by Kyle Min
 // Institution: Graz University of Technology
 // Email: feichtenhofer@tugraz
 // Date: Nov. 2015
-// [1] T. Brox, A. Bruhn, N. Papenberg, J. Weickert. High accuracy optical flow estimation based on a theory for warping. ECCV 2004.
-// [2] C. Zach, T. Pock, H. Bischof: A duality based approach for realtime TV-L 1 optical flow. DAGM 2007.
+// [1] C. Zach, T. Pock, H. Bischof: A duality based approach for realtime TV-L 1 optical flow. DAGM 2007.
 //************************************************************************
 
 #include <stdlib.h>
@@ -31,41 +30,8 @@
 
 using namespace std;
 using namespace cv;
-using namespace cv::gpu;
 
-float MIN_SZ = 256;
-int dilation = 1;
-float bound = 20;
-
-// Global variables for gpu::BroxOpticalFlow
-const float alpha_ = 0.197;
-const float gamma_ = 50;
-const float scale_factor_ = 0.8;
-const int inner_iterations_ = 10;
-const int outer_iterations_ = 77;
-const int solver_iterations_ = 10;
-
-void converFlowMat(Mat& flowIn, Mat& flowOut,float min_range_, float max_range_)
-{
-    float value = 0.0f;
-    for(int i = 0; i < flowIn.rows; i++)
-    {
-        float* Di = flowIn.ptr<float>(i);
-        char* Ii = flowOut.ptr<char>(i);
-        for(int j = 0; j < flowIn.cols; j++)
-        {
-            value = (Di[j]-min_range_)/(max_range_-min_range_);
-
-            value *= 255;
-            value = cvRound(value);
-
-            Ii[j] = (char) value;
-        }
-    }
-}
-
-static void convertFlowToImage(const Mat &flowIn, Mat &flowOut,
-        float lowerBound, float higherBound) {
+static void convertFlowToImage(const Mat &flowIn, Mat &flowOut, float lowerBound, float higherBound){
     #define CAST(v, L, H) ((v) > (H) ? 255 : (v) < (L) ? 0 : cvRound(255*((v) - (L))/((H)-(L))))
     for (int i = 0; i < flowIn.rows; ++i) {
         for (int j = 0; j < flowIn.cols; ++j) {
@@ -76,225 +42,148 @@ static void convertFlowToImage(const Mat &flowIn, Mat &flowOut,
     #undef CAST
 }
 
-int matMean(cv:: Mat img) {
-    int sum = 0;
-    int count = 0;
-    for (int r = 0; r < img.rows; r++) {
-        for (int c = 0; c < img.cols; c++) {
-            sum+=img.at<uchar>(c,r);
-            count++;
-        }
-    }
-    int mean = sum/count;
-    return mean;
-}
-
-int main( int argc, char *argv[] )
-{
-    GpuMat frame0GPU, frame1GPU, uGPU, vGPU;
-    Mat currentFrame, resizedFrame;
+int main( int argc, char *argv[] ){
+    cv::gpu::GpuMat frame0GPU, frame1GPU, uGPU, vGPU;
+    Mat currentFrame;
     Mat frame0_rgb_, frame1_rgb_, frame0_rgb, frame1_rgb, frame0, frame1, rgb_out;
     Mat frame0_32, frame1_32, imgU, imgV;
     Mat motion_flow, flow_rgb;
 
-    std::string input_folder, img_format, flow_format; //input video file name
-    int gpuID = 0,
-        type = 1,
-        stride = 1,
-        dilation = 1;
+    string img_format = "img_%07d.jpg", flow_format = "flow_%07d.jpg";
 
     const char* keys = {
                 "{ h  | help     | false | print help message }"
                 "{ g  | gpuID    |  0    | use this gpu}"
-                "{ f  | type     |  1    | use this flow method [0:Brox, 1:TVL1]}"
-                "{ s  | stride   |  1    | temporal stride (this subsamples the video)}"
-                "{ d  | dilation |  1    | temporal dilation (1: use neighbouring frames, 2: skip one)}"
-                "{ b  | bound    |  20   | maximum optical flow for clipping}"
-                "{ z  | size     |  256  | minimum output size}"
+                "{ d  | dilation |  3    | temporal dilation (1: use neighbouring frames, 2: skip one, 3: skip two)}"
+                "{ b  | bound    |  25   | maximum optical flow for clipping}"
     };
 
     CommandLineParser cmd(argc, argv, keys);
 
     if (cmd.get<bool>("help"))
     {
-        cout << "Usage: compute_flow input_folder img_formati flow_format [options]" << endl;
+        cout << "Usage: compute_flow input_folder gpu_id [options]" << endl;
         cout << "Available options:" << endl;
         cmd.printParams();
-        cout << "Example: compute_flow P01_01 img_%07d.jpg flow_%s_%07d.jpg [options]" << endl;
+        cout << "Example: compute_flow /z/dat/Kinetics list_dir_1.txt 0 [options]" << endl;
         return 0;
     }
 
-    if (argc > 2) {
-        input_folder=argv[1];
-        img_format=argv[2];
-        flow_format=argv[3];
+    string input_folder, input_file;
+    int gpuID = 0, dilation = 3, bound = 25;
 
-        gpuID = cmd.get<int>("gpuID");
-        type = cmd.get<int>("type");
-        stride = cmd.get<int>("stride");
+    if (argc > 3) {
+        input_folder = argv[1];
+        input_file = argv[2];
+        gpuID = atoi(argv[3]);
         dilation = cmd.get<int>("dilation");
         bound = cmd.get<float>("bound");
-        MIN_SZ = cmd.get<int>("size");
-    } else {
-        cout << "Not enough parameters!"<<endl;
-        cout << "Usage: compute_flow input_folder img_formati flow_format [options]" << endl;
+
+    }
+    else {
+        cout << "Not enough parameters!"<< endl;
+        cout << "Usage: compute_flow input_folder gpu_id [options]" << endl;
         cout << "Available options:" << endl;
         cmd.printParams();
-        cout << "Example: compute_flow P01_01 img_%07d.jpg flow_%s_%07d.jpg [options]" << endl;
+        cout << "Example: compute_flow /z/dat/Kinetics list_dir_1.txt 0 [options]" << endl;
         return 0;
     }
+
     int gpuCounts = cv::gpu::getCudaEnabledDeviceCount();
     cout << "Number of GPUs present " << gpuCounts << endl;
 
     cv::gpu::setDevice(gpuID);
     cv::gpu::printShortCudaDeviceInfo(cv::gpu::getDevice());
-
-    cv::gpu::BroxOpticalFlow dflow(alpha_,gamma_,scale_factor_,inner_iterations_,outer_iterations_,solver_iterations_);
     cv::gpu::OpticalFlowDual_TVL1_GPU alg_tvl1;
 
-    std::cout << "Processing folder " << input_folder << std::endl;
-
-    std::queue<cv::Mat> framequeue;
-
-    VideoCapture cap; //open video
-    try {
-        cap.open(input_folder+'/'+img_format);
-    } catch (std::exception& e) {
-        std::cout << e.what() << '\n'; //print exception
-    }
-
-    int nframes = 0, width = 0, height = 0;
-    float factor = 0;
-
-    if( cap.isOpened() == 0 ) {
-        cout << "Problem opening input file"<<endl;
-        return -1; //exit with a -1 error
-    }
-
-    int frame_to_write=1;
-    //NOW THE VIDEO IS OPEN AND WE CAN START GRABBING FRAMES
-
-    //fill the frame queue
-    for (int ii=0; ii<dilation; ii++) {
-        cap >> currentFrame;
-
-        factor = std::max<float>(MIN_SZ/currentFrame.cols, MIN_SZ/currentFrame.rows);
-
-        width = std::floor(currentFrame.cols*factor);
-        width -= width%2;
-        height = std::floor(currentFrame.rows*factor);
-        height -= height%2;
-
-        resizedFrame = cv::Mat(Size(width,height),CV_8UC3);
-
-        cv::resize(currentFrame,resizedFrame,cv::Size(width,height),0,0,INTER_CUBIC);
-
-        framequeue.push(resizedFrame.clone());
-    }
-
-    // cout << "Queue full" << endl;
-
-    while (!currentFrame.empty()){
-        resizedFrame = cv::Mat(Size(width,height),CV_8UC3);
-        cv::resize(currentFrame,resizedFrame,cv::Size(width,height),0,0,INTER_CUBIC);
-
-        framequeue.push(resizedFrame.clone());
-
-        //extract frame 0 from the front of the queue and pop
-        frame0_rgb = framequeue.front().clone();
-        framequeue.pop();
-
-        // Allocate memory for the images
-        //frame0_rgb = cv::Mat(Size(width,height),CV_8UC3);
-        flow_rgb = cv::Mat(Size(width,height),CV_8UC3);
-        motion_flow = cv::Mat(Size(width,height),CV_8UC3);
-        frame0 = cv::Mat(Size(width,height),CV_8UC1);
-        frame1 = cv::Mat(Size(width,height),CV_8UC1);
-        frame0_32 = cv::Mat(Size(width,height),CV_32FC1);
-        frame1_32 = cv::Mat(Size(width,height),CV_32FC1);
-
-        // Convert the image to grey and float
-        cvtColor(resizedFrame,frame1,CV_BGR2GRAY);
-        frame1.convertTo(frame1_32,CV_32FC1,1.0/255.0,0);
-
-        cvtColor(frame0_rgb,frame0,CV_BGR2GRAY);
-        frame0.convertTo(frame0_32,CV_32FC1,1.0/255.0,0);
-
-        switch(type){
-            case 0:
-                frame1GPU.upload(frame1_32);
-                frame0GPU.upload(frame0_32);
-                dflow(frame0GPU,frame1GPU,uGPU,vGPU);
-            case 1:
-                frame1GPU.upload(frame1);
-                frame0GPU.upload(frame0);
-                alg_tvl1(frame0GPU,frame1GPU,uGPU,vGPU);
+    string fsrc = "Kinetics-400-frames/", ftrg = "Kinetics-400-flow/";
+    string pf;
+    ifstream file((input_folder+"/"+input_file).c_str());
+    while(getline(file, pf)){
+        if (pf[pf.length()-1] == '\n') {
+            pf.erase(pf.length()-1);
         }
 
-        uGPU.download(imgU);
-        vGPU.download(imgV);
+        cout << "Start processing: " << pf << endl;
 
-        //cv::resize(imgU,imgU,cv::Size(width_out,height_out),0,0,INTER_CUBIC);
-        //cv::resize(imgV,imgV,cv::Size(width_out,height_out),0,0,INTER_CUBIC);
+        queue<Mat> framequeue;
+        VideoCapture cap; //open video
+        try{
+            cap.open(input_folder+"/"+fsrc+pf+"/"+img_format);
+        }
+        catch (exception& e){
+            cout << e.what() << endl; //print exception
+        }
 
-        float min_u_f = -bound;
-        float max_u_f = bound;
+        int width = 0, height = 0;
+        if( cap.isOpened() == 0 ){
+            cout << "Unable to capture" << endl;
+            return -1; //exit with a -1 error
+        }
 
-        float min_v_f = -bound;
-        float max_v_f = bound;
+        int frame_to_write = 1;
 
-        cv::Mat img_u(imgU.rows, imgU.cols, CV_8UC1);
-        cv::Mat img_v(imgV.rows, imgV.cols, CV_8UC1);
+        //NOW THE VIDEO IS OPEN AND WE CAN START GRABBING FRAMES
 
-        convertFlowToImage(imgU, img_u, min_u_f, max_u_f);
-        convertFlowToImage(imgV, img_v, min_v_f, max_v_f);
+        //fill the frame queue
+        for (int ii = 0; ii < dilation; ii++){
+            cap >> currentFrame;
+            width = currentFrame.cols;
+            height = currentFrame.rows;
+            framequeue.push(currentFrame.clone());
+        }
 
-        //if(!fout.isOpened()) {
-        //    fout.open(out_video,CV_FOURCC('M','P','4','2'),cap.get(CV_CAP_PROP_FPS),cv::Size(3*width,height),true);
-        //}
+        cap >> currentFrame;
+        while (!currentFrame.empty()){
+            //extract frame 0 from the front of the queue and pop
+            frame0_rgb = framequeue.front().clone();
+            framequeue.pop();
 
-        //if(!fout.isOpened()) {
-        //    cout << "Problem opening output file"<<endl;
-        //    return -1;
-        //}
+            // Allocate memory for the images
+            flow_rgb = Mat(Size(width,height),CV_8UC3);
+            motion_flow = Mat(Size(width,height),CV_8UC3);
+            frame0 = Mat(Size(width,height),CV_8UC1);
+            frame1 = Mat(Size(width,height),CV_8UC1);
+            frame0_32 = Mat(Size(width,height),CV_32FC1);
+            frame1_32 = Mat(Size(width,height),CV_32FC1);
 
-        //cv::resize(frame1_rgb,rgb_out,cv::Size(width_out,height_out),0,0,INTER_CUBIC);
+            // Convert the image to grey and float
+            cvtColor(currentFrame,frame1,CV_BGR2GRAY);
+            frame1.convertTo(frame1_32,CV_32FC1,1.0/255.0,0);
 
-        //Mat comb, comb2, img_uc, img_vc;
+            cvtColor(frame0_rgb,frame0,CV_BGR2GRAY);
+            frame0.convertTo(frame0_32,CV_32FC1,1.0/255.0,0);
 
-        //cvtColor(img_u,img_uc,CV_GRAY2BGR);
-        //cvtColor(img_v,img_vc,CV_GRAY2BGR);
+            frame1GPU.upload(frame1);
+            frame0GPU.upload(frame0);
+            alg_tvl1(frame0GPU,frame1GPU,uGPU,vGPU);
 
-        //horizontally stack flow, then rgb
-        //hconcat(img_uc,img_vc,comb);
-        //hconcat(resizedFrame,comb,comb2);
+            uGPU.download(imgU);
+            vGPU.download(imgV);
 
-        char *filename_x = new char[255];
-        char *filename_y = new char[255];
-        sprintf(filename_x,flow_format.c_str(),"x",frame_to_write);
-        sprintf(filename_y,flow_format.c_str(),"y",frame_to_write);
-        cv::imwrite(input_folder + "/" + string(filename_x),img_u);
-        cv::imwrite(input_folder + "/" + string(filename_y),img_v);
+            float min_u_f = -bound;
+            float max_u_f = bound;
 
-        frame_to_write++;
+            float min_v_f = -bound;
+            float max_v_f = bound;
 
-        //fout.write(comb2);
-        cout << "." << flush;
+            Mat img_u(imgU.rows, imgU.cols, CV_8UC1);
+            Mat img_v(imgV.rows, imgV.cols, CV_8UC1);
 
-        //frame1_rgb.copyTo(frame0_rgb);
-        //cvtColor(frame0_rgb,frame0,CV_BGR2GRAY);
-        //frame0.convertTo(frame0_32,CV_32FC1,1.0/255.0,0);
+            convertFlowToImage(imgU, img_u, min_u_f, max_u_f);
+            convertFlowToImage(imgV, img_v, min_v_f, max_v_f);
 
-        //skip exactly frameSkip frames
-        nframes++;
-        for (int iskip = 0; iskip<stride-1; iskip++) {
+            char *flowname = new char[255];
+            sprintf(flowname, flow_format.c_str(), frame_to_write);
+
+            imwrite(input_folder+"/"+ftrg+pf+"/u/"+string(flowname),img_u);
+            imwrite(input_folder+"/"+ftrg+pf+"/v/"+string(flowname),img_v);
+
+            frame_to_write++;
+            framequeue.push(currentFrame.clone());
             cap >> currentFrame;
         }
-        cap >> currentFrame;
     }
-
-    cout << endl;
-
     return 0;
 }
 
